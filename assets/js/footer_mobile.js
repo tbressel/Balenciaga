87e4f424c40cff7c50e6b0d41532016efdf6ef22
tbressel <tbressel.dev@gmail.com>
2023-08-05 20:10:44 +0200

    Animation et fonctionnement du footer mobile terminé

--- assets/js/footer_mobile.js
+++ assets/js/footer_mobile.js
@@ -3,16 +3,32 @@ console.log(ELEMENT__footer);
 ELEMENT__footer.forEach(element => {
     element.addEventListener('click', () => {
 
+        HideAll_footer();
+        
+        let ELEMENT__value =  element.querySelector(".footer_level2").classList[1];
+        console.log(ELEMENT__value);
+        
         
 
-        let ELEMENT__value = element.querySelector(".footer_level2").classList[1];
-        console.log(ELEMENT__value);
-        if(ELEMENT__value === "display__false") {
+        if(ELEMENT__value === "display__swingfalse") {
             HideAll_level2();
-            element.querySelector(".footer_level2").classList.replace("display__false", "display__true")
+            element.querySelector(".footer_level2").classList.replace("display__swingfalse", "display__swingtrue")
         } else {
-            element.querySelector(".footer_level2").classList.replace("display__true", "display__false")
+            element.querySelector(".footer_level2").classList.replace("display__swingtrue", "display__swingfalse")
         }
+
+        
     })
     
+    
 });
+
+
+
+function HideAll_footer() {
+    ELEMENT__hide = document.querySelectorAll(".footer_level2");
+   ELEMENT__hide.forEach(element => {
+       element.classList.replace("display__swingtrue", "display__swingfalse");
+       console.warn("JE suis rentré dans la fonction HideAll et je récupère çà :",ELEMENT__hide )
+   })
+}
